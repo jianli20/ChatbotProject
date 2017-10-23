1ae35f15027fd9c3fb606ac13f373a07662af72f
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-10-23 11:59:51 -0600

    added a basic popup, added image

--- src/chat/controller/ChatRunner.java
+++ src/chat/controller/ChatRunner.java
@@ -1,6 +1,15 @@
 package chat.controller;
 
+import chat.view.PopupDisplay;
+
 public class ChatRunner
 {
-
+	public static void main (String [] args)
+	{
+		PopupDisplay test = new PopupDisplay();
+		ChatController app = new ChatController();
+				app.start();
+		test.displayText("hi");
+		
+	}
 }
