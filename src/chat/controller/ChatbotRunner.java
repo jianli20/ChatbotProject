39685559d8e24d3de46330fb6ff3feedcd533121
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-10-23 13:37:40 -0600

    added length checker, add tests

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -2,12 +2,12 @@ package chat.controller;
 
 import chat.view.PopupDisplay;
 
-public class ChatRunner
+public class ChatbotRunner
 {
 	public static void main (String [] args)
 	{
 		PopupDisplay test = new PopupDisplay();
-		ChatController app = new ChatController();
+		ChatbotController app = new ChatbotController();
 				app.start();
 		test.displayText("hi");
 		
