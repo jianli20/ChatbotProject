4dc3df16c64255b8bbc3331e4fd87ecb753ae813
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-13 13:45:30 -0700

    Made Panel class

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,17 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
+{
+	private ChatAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+}
