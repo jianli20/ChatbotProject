19e5edd05ece82d6cc36542fdefe905384dbd7e8
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-15 12:26:37 -0700

    Added ChatFrame to the controller class, commented out while loop

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,28 +2,31 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 {
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appframe;
 
 	
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Squarebot");
 		display = new PopupDisplay();
+		appframe = new ChatFrame(this);
 	}
 	
 	public void start()
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response = display.collectResponse(response);
+//		}
 	}
 	
 //	public interactWithChatbot(String input)
