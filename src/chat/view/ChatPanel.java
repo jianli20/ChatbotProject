018d16a9eca98a42dd652d7eb558d8b9a0395f6b
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-11-17 12:19:24 -0700

    Added code to get text, process it, and then set the textbox blank

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -78,7 +78,10 @@ public class ChatPanel extends JPanel
 			{
 				public void actionPerformed(ActionEvent click) 
 				{
-					
+					String userText = inputField.getText();
+					String displayText = appController.interactWithChatbot(userText);
+					chatArea.append(displayText);
+					inputField.setText("");
 				}
 			});
 	}
